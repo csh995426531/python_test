0fb1050d015129d5d71f4007698f0c54d26be58a
csh995426531 <15670297001@163.com>
2017-11-01 13:55:23 +0800

    新增function_default.py和function_keyword.py

---
+++ function_default.py
@@ -0,0 +1,4 @@
+def say(message,times=2):
+	print(message*times)
+say('hello ')
+say('hello ',5)
---
+++ function_keyword.py
@@ -0,0 +1,5 @@
+def func(a,b=1,c=2):
+	print('a is',a,'and b is',b,'and c is',c)
+func(11,2)
+func(c=4,a=0)
+func(111,c=222,b=22)
