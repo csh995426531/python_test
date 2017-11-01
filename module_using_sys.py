8f71733c20e7d12ddf525fcbaf5e78f11aaf72ae
csh995426531 <15670297001@163.com>
2017-11-02 07:10:45 +0800

    module_using_sys.py更新from..impoort

--- module_using_sys.py
+++ module_using_sys.py
@@ -1,7 +1,10 @@
-import sys
+# import sys
 
-print('The command line arguments are:')
-for i in sys.argv:
-	print(i)
+# print('The command line arguments are:')
+# for i in sys.argv:
+# 	print(i)
 
-print('\n\nThe PYTHONPATH is',sys.path,'\n')
+# print('\n\nThe PYTHONPATH is',sys.path,'\n')
+
+from math import sqrt
+print('Square root of 16 is',sqrt(16))
