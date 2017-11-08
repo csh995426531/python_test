fd79b80bd126a87a2073d6886b8cd7c963553ee3
csh995426531 <15670297001@163.com>
2017-11-08 15:46:42 +0800

    *号,和**号

---
+++ powersum.py
@@ -0,0 +1,11 @@
+def powersum(power, *args):
+	'''Return the sum of each argument raised to the specified power.'''
+	total = 0
+	for i in args:
+		total += pow(i, power)
+	return total
+	
+print(powersum(2, 3, 4))
+	
+print(powersum(2, 10))
+	
